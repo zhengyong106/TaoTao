e44fc34f47fc94fa1aceeb895834844817775fdc
zhengyong <1192133596@qq.com>
2019-06-30 00:24:08 +0800

    first commit!

--- taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemCategoryController.java
+++ taotao-manage/taotao-manage-web/src/main/java/com/taotao/manage/controller/ItemCategoryController.java
@@ -1,27 +1,29 @@
 package com.taotao.manage.controller;
 
-import com.taotao.manage.bean.LayuiResult;
-import com.taotao.manage.pojo.ItemCat;
-import com.taotao.manage.service.ItemCatService;
+import com.taotao.manage.pojo.ItemCategory;
+import com.taotao.manage.service.ItemCategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @RequestMapping("item/cat")
 @Controller
-public class ItemCatController {
+public class ItemCategoryController {
     @Autowired
-    private ItemCatService itemCatService;
+    private ItemCategoryService itemCategoryService;
 
     @GetMapping
-    public ResponseEntity queryItemCatList(){
+    public ResponseEntity queryItemCategoryList(@RequestParam(value = "parentId", defaultValue = "0") Long parentId){
         try{
-            List<ItemCat> list = itemCatService.queryItemCatListByParentId(0L);
+            ItemCategory record = new ItemCategory();
+            record.setParentId(parentId);
+            List<ItemCategory> list = itemCategoryService.queryList(record);
             if(null == list || list.isEmpty()){
                 // 资源不存在
                 return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
